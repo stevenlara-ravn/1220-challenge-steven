cdc1ccabf28d3ca7ca835c305423830affb99065
Steven Lara <stevencarcamo@ravn.co>
2024-12-20 10:17:27 -0600

    chore: Setup Apollo with Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { gql, useQuery } from "@apollo/client"
+
+const getCharacters = gql`
+  query {
+  characters {
+    results {
+      id
+      name
+      image
+      status
+    }
+  }
+  }
+`
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { loading, error, data } = useQuery(getCharacters)
 
+  
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+
+  console.log(data);
+  
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h1>Hello Vite!</h1>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+
+const client = new ApolloClient({
+    uri: "https://rickandmortyapi.com/graphql",
+    cache: new InMemoryCache(),
+})
+
+
 createRoot(document.getElementById('root')!).render(
+   <ApolloProvider client={client}>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </ApolloProvider>
 )
